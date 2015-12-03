cb0de3f72530929a381a82c597ebfaddfe4bebc1
Tess Thyer <tthyer@zulily.com>
2015-12-03 13:18:04 -0800

    Initial logic commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # require-glob
+An elaboration of a stack overflow comment on requiring groups of files. This allows you to require a directory of files using a [glob](https://github.com/isaacs/node-glob) pattern, optionally execute it, and returns reference objects containing name, path, module, and results.
---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const glob = require('glob');
+const path = require('path');
+
+const internals = {};
+
+internals.indexBy = function(arr, index) {
+  let result = {};
+  arr.forEach(function(member) {
+    result[member[index]] = member;
+  });
+  return result;
+};
+
+module.exports = function(pattern, options) {
+  let requirements = glob.sync(pattern).map(function(file) {
+    let requirement = {};
+    requirement.path = file;
+    requirement.name = path.basename(file).replace(path.extname(file), '');
+    requirement.module = require(path.resolve(file));
+    if(options && options[requirement.name] && options[requirement.name] instanceof Array) {
+      requirement.results = requirement.module.apply(options[requirement.name]);
+    }
+    return requirement;
+  });
+  return internals.indexBy(requirements, 'name');
+};
